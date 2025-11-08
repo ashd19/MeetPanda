0b9a885f34a69a925b1fa81aba0aa14b3ef156e1
Ashton <ashtondsouza192@gmail.com>
2025-11-09 01:43:01 +0530

    Latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Footer from "./components/Footer";
 import UseCases from "./components/UseCases";
 import GetStarted from "./components/GetStarted";
-import Benefits from "./components/Benefits";
+import Benefits from "./components/Benefits"; 
+import SocialProof from "./components/SocialProof";
+
 function App() {
   return (
     <div>
       <Benefits />
       <GetStarted />
       <UseCases />
+      <SocialProof />
       <Footer />
     </div>
   );
---
+++ src/components/SocialProof.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { CircleQuestionMark } from "lucide-react";
+function SocialProof() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-5">
+      <div className="first">
+        <div className="flex items-center gap-1.5 text-xs max-w-fit font-medium text-neutral-800 bg-primary/10 border-white border-primary/20 rounded-full bg-purple-200 px-2 py-1">
+          <CircleQuestionMark className="w-[12px] h-[13px]" /> Got Questions?
+        </div>
+      </div>
+      <div className="">
+        <h1 className="text-5xl  font-bold flex">
+          Backed by Our <h1 className="text-blue-700"> Growing Community</h1>
+        </h1>
+      </div>
+      <div>
+        <h1>
+          Be part of the movement towards smarter, more efficient meetings.
+        </h1>
+      </div>{" "}
+      <div>
+        <h1>Trusted by 100+ professionals and growin</h1>
+      </div>
+      {/*  Cards , 1] classic props !!! 2] animation ! doable !!! */}
+    </div>
+  );
+}
+
+export default SocialProof;
