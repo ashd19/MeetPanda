fdeb1e08ad77f001661a62079fc99331da945008
Ashton <ashtondsouza192@gmail.com>
2025-11-10 11:05:12 +0530

    recent changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Footer from "./components/Footer";
 import UseCases from "./components/UseCases";
 import GetStarted from "./components/GetStarted";
-import Benefits from "./components/Benefits"; 
+import Benefits from "./components/Benefits";
 import SocialProof from "./components/SocialProof";
+import Workflow from "./components/WorkFlow";
 
 function App() {
   return (
     <div>
       <Benefits />
       <GetStarted />
+      <Workflow />
       <UseCases />
       <SocialProof />
       <Footer />
--- src/components/WorkFlow.jsx
+++ src/components/WorkFlow.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Sparkle } from "lucide-react";
 
 function Workflow() {
-  return <div> Workflow</div>;
+  return (
+    <div className="flex flex-col items-center px-2 gap-5">
+      <p className=" inline-flex gap-2 bg-indigo-50  text-indigo-600 px-3 py-1 rounded-full text-sm mb-4">
+        <Sparkle width={10} height={10} className="mt-1" />
+        Our WorkFlow
+      </p>
+      <h1 className="text-3xl font-bold">
+        Your <span className="text-blue-800 font-bold"> AI Teammate</span>,
+        Always in the room
+      </h1>
+      <h1 className="text-lg">
+        MeetPanda's AI seamlessly integrates into your favourite meeting
+        platforms—whether it's Google Meet, <br /> or Microsoft Teams. No
+        installs, no disruptions.
+      </h1>
+      <div className="flex items-center gap-5">
+
+        
+      </div>
+    </div>
+  );
 }
 
 export default Workflow;
