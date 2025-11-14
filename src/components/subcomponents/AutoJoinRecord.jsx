19ce441670ddef8a3d251df57e6e1786161c61f0
Ashton <ashtondsouza192@gmail.com>
2025-11-14 14:34:52 +0530

    recent changes:

--- src/components/subcomponents/AutoJoinRecord.jsx
+++ src/components/subcomponents/AutoJoinRecord.jsx
@@ -4,11 +4,11 @@ function AutoJoinRecord() {
   return (
     <div className="bg-white rounded-xl md:w-[550px]  p-5 shadow-sm border border-gray-100">
       <h3 className="font-semibold mb-2">Auto-Join &amp; Record</h3>
-      <p className="text-gray-500 mb-6">
+      <p className="text-gray-500 ">
         Hands-free calendar sync: MeetPanda joins your calls without downloads
         or bots.
       </p>
-      <div className="flex justify-center">
+      <div className="flex justify-center mt-5 md:mt-[100px]">
         <img src="https://meetpanda.in/img/home/benefits-autojoin.svg" alt="" />
       </div>
     </div>
